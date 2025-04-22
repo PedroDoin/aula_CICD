7491d0abf1e6df8792a4a2b8be5f326ff3b3f020
PedroDoin <pedrohdoin2@gmail.com>
2025-04-22 20:59:26 -0300

    Update github.c

--- github.c
+++ github.c
@@ -1,7 +1,10 @@
-#include <stdio.h>
+  #include <stdio.h>
 
-  int main() {
-  printf("Olá, GitHub Actions!\n");
-return 0; 
-
+int main() {
+    int num1, num2, num3
+num1 = 10
+num2 = 5
+num3 = num1 + num2
+printf("a soma de %d e %d é: %d\n", num1, num2, num3);
+return 0;
 }
