b8b5a3aec67bf116c039f1f7b3f4e0600a5db500
PedroDoin <pedrohdoin2@gmail.com>
2025-04-22 21:08:39 -0300

    Update github.c

--- github.c
+++ github.c
@@ -1,10 +1,10 @@
   #include <stdio.h>
 
 int main() {
-    int = (num1, num2, num3);
-num1 = 10
-num2 = 5
-num3 = num1 + num2
+    int  num1, num2, num3;
+num1 = 10;
+num2 = 5;
+num3 = num1 + num2;
 printf("a soma de %d e %d é: %d\n", num1, num2, num3);
 return 0;
 }
